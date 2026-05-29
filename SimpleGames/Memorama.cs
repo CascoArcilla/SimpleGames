57e818610465f49f9005e73840c725effca07b7f
CascoArcilla <normon1230@gmail.com>
2026-05-29 13:08:01 -0600

    refactorizando el contructor del memorama

--- SimpleGames/Memorama.cs
+++ SimpleGames/Memorama.cs
@@ -8,10 +8,16 @@ namespace Games
         public string Name => "Memorama";
 
         private readonly string[] charactersValues = new string[] { "S", "D", "F", "H" };
-        private List<Piece> Pieces;
+        private List<Piece> _pieces;
+        private Piece[,] _structurePieces;
 
-        public Memorama() 
+        private Piece _first;
+        private Piece _second;
+        private string _message;
+        private readonly int _dimension = 4;
+        public Memorama()
         {
+            // Crear piezas del memorama
             List<Piece> piecesSorted = new List<Piece>();
             foreach (string item in this.charactersValues)
             {
@@ -21,13 +27,29 @@ namespace Games
                 piecesSorted.Add(new Piece(item));
             }
 
-            this.Pieces = ListExtensions.Shuffle(piecesSorted);
+            // Revolver las piezas
+            this._pieces = ListExtensions.Shuffle(piecesSorted);
+
+            // Crear la estructura del tablero 4x4
+            _structurePieces = new Piece[this._dimension, this._dimension];
+
+            // Agregar las piezas a la estructura del tablero
+            int index = 0;
+            for (int i = 0; i < this._dimension; i++)
+            {
+                for (int j = 0; j < this._dimension; j++)
+                {
+                    this._structurePieces[i, j] = this._pieces[index];
+                    index++;
+                }
+            }
         }
 
         public void Play()
         {
-            Console.WriteLine($"Jugando {this.Name} ahora");
+            Console.WriteLine($"Jugando {this.Name} ahora\n");
             this.ShowBoard();
+            this.ShowMenu();
         }
 
         private void ShowBoard()
@@ -38,7 +60,7 @@ namespace Games
             string incognit = "?";
             int checkIndex = 0;
 
-            foreach (Piece item in this.Pieces)
+            foreach (Piece item in this._structurePieces)
             {
                 switch (checkIndex)
                 {
@@ -60,13 +82,70 @@ namespace Games
                         break;
                 }
 
-                string show = item.Revealed ? item.Value : incognit;
+                string show = !item.Revealed ? item.Value : incognit;
                 Console.Write($" {show} |");
 
                 checkIndex++;
             }
 
             Console.WriteLine("\n   --- --- --- ---");
+        }
+
+        private void ShowMenu()
+        {
+            Console.WriteLine("Se ingresa primero una casila y despues la siguiente.");
+            Console.WriteLine("Estructura de casilla: [posicion_x][espacio][posicion_y]");
+            Console.WriteLine("Ej.\"1 1\", \"2 3\", \"4 2\"");
+
+            if (this._first != null) Console.WriteLine($"Primera casilla: {this._first.Value}");
+            if (this._second != null) Console.WriteLine($"Segunda casilla: {this._second.Value}");
+            if (this._message != null || this._message != "") Console.WriteLine(this._message);
+        }
+
+        private bool ReadValidateInput()
+        {
+            string input;
+            bool isValid;
+            int posX;
+            int posY;
+            string message;
+
+            do
+            {
+                Console.WriteLine("Intenta adivinar el numero de uno de los caracteres.");
+                Console.Write("Ingrese respuesta: ");
+                input = Console.ReadLine() ?? string.Empty;
+
+                (isValid, posX, posY, message) = ValidateInputText(input);
+
+                if (isValid) break;
+
+                Console.WriteLine($"Entrada no valida. {message}");
+            }
+            while (true);
+
+            var success = SetPieces(posX, posY);
+
+            return success;
+        }
+
+        private Tuple<bool, int, int, string> ValidateInputText(string response)
+        {
+            var parts = response.Trim().Split(' ');
+
+            if (parts.Length != 2) return Tuple.Create(false, 0, 0, "Su repuesta puede no tener espacio");
+            if (!int.TryParse(parts[0], out int px)) return Tuple.Create(false, 0, 0, "Las entradas deben ser numeros");
+            if (!int.TryParse(parts[1], out int py)) return Tuple.Create(false, 0, 0, "Las entradas deben ser numeros");
+            if (px < 1 || px > this._dimension || py < 1 || py > this._dimension) return Tuple.Create(false, 0, 0, "Una entrada esta fuera de rango");
+
+            return Tuple.Create(true, px, py, "Validacion de entrada exitosa.");
+        }
+
+        private bool SetPieces(int posX, int posY)
+        {
+            bool success = true;
+
+            return success;
         }
     }
 
